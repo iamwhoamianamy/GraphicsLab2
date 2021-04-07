f927965eb2727b8ad3d882aca27c01847898ef7a
zadolbali10 <anon.yu@mail.ru>
2021-04-07 17:55:31 +0700

    Implemented basic methodes

---
+++ Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace GraphicsLab2
+{
+   class Game : GameWindow
+   {
+      public Game(int width, int height, string title) :
+           base(width, height, GraphicsMode.Default, title)
+      {
+
+      }
+
+      protected override void OnLoad(EventArgs e)
+      {
+
+
+         base.OnLoad(e);
+      }
+
+      protected override void OnRenderFrame(FrameEventArgs e)
+      {
+         GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+         Context.SwapBuffers();
+         base.OnRenderFrame(e);
+      }
+
+      protected override void OnResize(EventArgs e)
+      {
+         GL.Disable(EnableCap.DepthTest);
+         GL.Viewport(0, 0, Width, Height);
+         GL.MatrixMode(MatrixMode.Projection);
+         GL.LoadIdentity();
+         GL.Ortho(0, Width, Height, 0, -1.0, 1.0);
+         GL.MatrixMode(MatrixMode.Modelview);
+         GL.LoadIdentity();
+
+         base.OnResize(e);
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsLab2
+{
+   class Program
+   {
+      public static void Main()
+      {
+         using (Game game = new Game(1200, 800, "LearnOpenTK"))
+         {
+
+            game.Run(60.0);
+         }
+      }
+   }
+}
